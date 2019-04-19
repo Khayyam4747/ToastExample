7236f054ce0af695b8f7628b7300000c25c65d76
khayyam.abdinov <khayyam.abdinov@voidaint.com>
2019-04-19 11:29:01 +0400

    Added Full Toast

--- app/src/main/java/com/khayyam/toastexample/MainActivity.java
+++ app/src/main/java/com/khayyam/toastexample/MainActivity.java
@@ -3,11 +3,17 @@ package com.khayyam.toastexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.khayyam.toasterlibrary.ToasterMessage;
+
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToasterMessage.s(MainActivity.this,"Ya Allah");
+
     }
 }
